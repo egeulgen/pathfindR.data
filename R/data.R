2aed9441ec458234bb99bfc5be4c8901f755486b
egeulgen <egeulgen@gmail.com>
2024-04-27 12:55:57 +0100

    update all data and documentation

--- R/data.R
+++ R/data.R
@@ -3,11 +3,13 @@
 #' Data frame containing all the data for pathfindR along with descriptions and
 #' last update dates.
 #'
-#' @format A data frame with 30 rows and 4 columns:
+#' @format A data frame with 30 rows and 6 columns:
 #' \describe{
 #'   \item{Category}{Category of the data}
 #'   \item{Name}{Name of the data}
 #'   \item{Description}{Description of the data}
+#'   \item{Source}{Source of the data}
+#'   \item{Version}{Version of the data (if applicable)}
 #'   \item{Last_Update}{Last update date}
 #' }
 "pathfindR.data_updates"
@@ -23,7 +25,7 @@
 #' antibody (treatment group) and the rest were treated with control IgG
 #' antibody (control group). In this dataset, differentially-expressed genes
 #' with |logFC| >= 2 and FDR < 0.05 are presented.
-#' \emph{Generated on Nov 1, 2019.}
+#' \emph{Generated on 1 Nov 2019.}
 #'
 #' @format A data frame with 45 rows and 2 variables: \describe{
 #'   \item{Gene_Symbol}{MGI gene symbols of the differentially-expressed genes}
@@ -40,9 +42,9 @@
 #' A dataset containing the results of pathfindR's active-subnetwork-oriented
 #' enrichment workflow performed on the Mus musculus myeloma
 #' differential expression dataset \code{\link{example_mmu_input}}.
-#' \emph{Generated on Dec 11, 2022.}
+#' \emph{Generated on 27 Apr 2024.}
 #'
-#' @format A data frame with 26 rows and 9 columns:
+#' @format A data frame with 34 rows and 9 columns:
 #' \describe{
 #'   \item{ID}{ID of the enriched term}
 #'   \item{Term_Description}{Description of the enriched term}
@@ -65,7 +67,7 @@
 #' genes for 18 rheumatoid arthritis (RA) patients and 15 healthy subjects. The
 #' matrix contains expression values of  572 significantly
 #' differentially-expressed genes (see \code{\link{example_pathfindR_input}}) with adj.P.Val <= 0.05.
-#' \emph{Generated on Sep 28, 2019.}
+#' \emph{Generated on 28 Sep 2019.}
 #'
 #' @format A matrix with 572 rows and 33 columns.
 #' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE15573}
@@ -84,7 +86,7 @@
 #' blood mononuclear cells of 18 rheumatoid arthritis (RA) patients versus 15
 #' healthy subjects. Differentially-expressed genes with adj.P.Val < 0.05 are
 #' presented in this data frame.
-#' \emph{Generated on Nov 1, 2019.}
+#' \emph{Generated on 1 Nov 2019.}
 #'
 #' @format A data frame with 572 rows and 3 variables: \describe{
 #'   \item{Gene.symbol}{HGNC gene symbols of the differentially-expressed genes}
@@ -105,9 +107,9 @@
 #' enrichment workflow performed on the rheumatoid arthritis
 #' differential-expression data frame \code{\link{example_pathfindR_input}}. Analysis via
 #' \code{run_pathfindR} was performed using the default settings.
-#' \emph{Generated on Dec 11, 2022.}
+#' \emph{Generated on 27 Apr 2024.}
 #'
-#' @format A data frame with 128 rows and 9 columns:
+#' @format A data frame with 121 rows and 9 columns:
 #' \describe{
 #'   \item{ID}{ID of the enriched term}
 #'   \item{Term_Description}{Description of the enriched term}
@@ -133,9 +135,9 @@
 #' function \code{\link[pathfindR]{cluster_enriched_terms}} was used with the default settings
 #' (i.e. hierarchical clustering was performed and the agglomeration method
 #' was "average").
-#' \emph{Generated on Dec, 2022.}
+#' \emph{Generated on 27 Apr 2024.}
 #'
-#' @format A data frame with 128 rows and 11 columns:
+#' @format A data frame with 121 rows and 11 columns:
 #' \describe{
 #'   \item{ID}{ID of the enriched term}
 #'   \item{Term_Description}{Description of the enriched term}
@@ -162,9 +164,9 @@
 #' enrichment workflow performed on the rheumatoid arthritis dataset GSE84074
 #' \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE84074}. Analysis via
 #' \code{run_pathfindR} was performed using the default settings.
-#' \emph{Generated on Dec 11, 2022.}
+#' \emph{Generated on 27 Apr 2024.}
 #'
-#' @format A data frame with 53 rows and 9 columns:
+#' @format A data frame with 38 rows and 9 columns:
 #' \describe{
 #'   \item{ID}{ID of the enriched term}
 #'   \item{Term_Description}{Description of the enriched term}
@@ -190,9 +192,9 @@
 #' is a vector of gene symbols located in the given pathway. Names correspond to
 #' the KEGG ID of the pathway. Pathways that did not contain any genes were
 #' discarded.
-#' \emph{Generated on Dec 10, 2022.}
+#' \emph{Generated on 27 Apr 2024.}
 #'
-#' @format list containing 341 vectors of gene symbols. Each vector corresponds
+#' @format list containing 358 vectors of gene symbols. Each vector corresponds
 #'   to a pathway.
 "kegg_genes"
 
@@ -201,9 +203,9 @@
 #' A named vector containing the descriptions for each Homo sapiens KEGG pathway. Names of the
 #' vector correspond to the KEGG ID of the pathway. Pathways that did not contain
 #' any genes were discarded.
-#' \emph{Generated on Dec 10, 2022.}
+#' \emph{Generated on 27 Apr 2024.}
 #'
-#' @format named vector containing 341 character values, the descriptions for the given
+#' @format named vector containing 358 character values, the descriptions for the given
 #'   pathways.
 "kegg_descriptions"
 
@@ -214,9 +216,9 @@
 #' is a vector of gene symbols located in the given pathway. Names correspond to
 #' the KEGG ID of the pathway. Pathways that did not contain any genes were
 #' discarded.
-#' \emph{Generated on Dec 10, 2022.}
+#' \emph{Generated on 27 Apr 2024.}
 #'
-#' @format list containing 336 vectors of gene symbols. Each vector corresponds
+#' @format list containing 355 vectors of gene symbols. Each vector corresponds
 #'   to a pathway.
 "mmu_kegg_genes"
 
@@ -225,9 +227,9 @@
 #' A named vector containing the descriptions for each Mus musculus KEGG pathway. Names of the
 #' vector correspond to the KEGG ID of the pathway. Pathways that did not contain
 #' any genes were discarded.
-#' \emph{Generated on Dec 10, 2022.}
+#' \emph{Generated on 27 Apr 2024.}
 #'
-#' @format named vector containing 336 character values, the descriptions for the given
+#' @format named vector containing 355 character values, the descriptions for the given
 #'   pathways.
 "mmu_kegg_descriptions"
 
@@ -236,9 +238,9 @@
 #' A list containing the genes involved in each human Reactome pathway. Each
 #' element is a vector of gene symbols located in the given pathway. Names
 #' correspond to the Reactome ID of the pathway.
-#' \emph{Generated on Dec 10, 2022.}
+#' \emph{Generated on 27 Apr 2024.}
 #'
-#' @format list containing 2593 vectors of gene symbols. Each vector corresponds
+#' @format list containing 2681 vectors of gene symbols. Each vector corresponds
 #'   to a pathway.
 "reactome_genes"
 
@@ -246,9 +248,9 @@
 #'
 #' A named vector containing the descriptions for each human Reactome pathway. Names of the
 #' vector correspond to the Reactome ID of the pathway.
-#' \emph{Generated on Dec 10, 2022.}
+#' \emph{Generated on 27 Apr 2024.}
 #'
-#' @format named vector containing 2593 character values, the descriptions for the given
+#' @format named vector containing 2681 character values, the descriptions for the given
 #'   pathways.
 "reactome_descriptions"
 
@@ -256,7 +258,7 @@
 #'
 #' A list containing the genes involved in each human BioCarta pathway. Each
 #' element is a vector of gene symbols located in the given pathway.
-#' \emph{Generated on Dec 10, 2022.}
+#' \emph{Generated on 27 Apr 2024.}
 #'
 #' @format list containing 292 vectors of gene symbols. Each vector corresponds
 #'   to a gene set.
@@ -265,7 +267,7 @@
 #' BioCarta Pathways - Descriptions
 #'
 #' A named vector containing the descriptions for each human BioCarta pathway.
-#' \emph{Generated on Dec 10, 2022.}
+#' \emph{Generated on 27 Apr 2024.}
 #'
 #' @format named vector containing 292 character values, the descriptions for the given
 #'   pathways.
@@ -280,7 +282,7 @@
 #' Hu C, Li T, Xu Y, Zhang X, Li F, Bai J, et al. CellMarker 2.0: an updated
 #' database of manually curated cell markers in human/mouse and web tools based
 #' on scRNA-seq data. Nucleic Acids Res. 2022 Oct 27;gkac947.
-#' \emph{Generated on Dec 10, 2022.}
+#' \emph{Generated on 27 Apr 2024.}
 #'
 #' @format list containing 1986 vectors. Each vector corresponds to a cell marker
 #' gene set for a given human cell type.
@@ -295,7 +297,7 @@
 #' Hu C, Li T, Xu Y, Zhang X, Li F, Bai J, et al. CellMarker 2.0: an updated
 #' database of manually curated cell markers in human/mouse and web tools based
 #' on scRNA-seq data. Nucleic Acids Res. 2022 Oct 27;gkac947.
-#' \emph{Generated on Dec 10, 2022.}
+#' \emph{Generated on 27 Apr 2024.}
 #'
 #' @format named vector containing 1986 character values, the descriptions for
 #' the given human cell types.
@@ -305,9 +307,9 @@
 #'
 #' A list containing the genes involved in each GO ontology term. Each
 #' element is a vector of gene symbols located in the given gene set.
-#' \emph{Generated on Dec 10, 2022.}
+#' \emph{Generated on 27 Apr 2024.}
 #'
-#' @format list containing 15508 vectors of gene symbols. Each vector corresponds
+#' @format list containing 15450 vectors of gene symbols. Each vector corresponds
 #'   to a GO gene set.
 "go_all_genes"
 
@@ -316,16 +318,16 @@
 #'
 #' A data frame consisting of pathfindR enrichment analysis results on the
 #' example TF target genes data (target gene sets of CREB and MYC).
-#' \emph{Generated on Aug 20, 2021.}
-#' @format data frame containing 2 rows and 8 columns. Each row is a gene set (the TF target gene sets).
+#' \emph{Generated on 27 Apr 2024.}
+#' @format data frame containing 2 rows and 9 columns. Each row is a gene set (the TF target gene sets).
 "example_custom_genesets_result"
 
 #' Example Active Subnetworks
 #'
 #' A list of vectors containing genes for each active subnetwork that passed
 #' the filtering step.
-#' \emph{Generated on Nov 1, 2019.}
+#' \emph{Generated on 27 Apr 2024.}
 #'
-#' @format list containing 112 vectors. Each vector is the set of genes for the
+#' @format list containing 150 vectors. Each vector is the set of genes for the
 #' given active subnetwork.
 "example_active_snws"
